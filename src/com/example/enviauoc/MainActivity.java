ae3de146f0bbe9aa5c216693fe63a0a077866cf2
sergioromerogarcia <sergioromero@Koloss.local>
2014-11-26 22:28:18 +0100

    DesdeCasa
    modificaciones en Main

--- src/com/example/enviauoc/MainActivity.java
+++ src/com/example/enviauoc/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.enviauoc;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	BaseDatos usuariosdb;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_enviauoc);
+		
+		//Creamos el objeto usuariodb de nuestra clase de Base de datos que utilizamos para crear la base de datos así como 
+        //los métodos principales para trabajar con los datos.
+        //Los parámetros los pasamos a null ya que los valores los tenemos definidos como constantes en la clase de Base de Datos
+		usuariosdb = new BaseDatos(this, null, null);
+		
 	}
 }
