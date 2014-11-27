94200c6f8f4ee02a0000737deff38e61b4622ded
sergioromerogarcia <sergio.romero.garcia@gmail.com>
2014-11-27 17:46:13 +0100

    Desde Atlantis 2

---
+++ src/com/example/enviauoc/Consulta_Envio.java
@@ -0,0 +1,15 @@
+package com.example.enviauoc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Consulta_Envio extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Fichero xml que define la interfaz grafica
+		setContentView(R.layout.consulta_envio);
+	}
+
+}
---
+++ src/com/example/enviauoc/Nuevo_Envio.java
@@ -0,0 +1,15 @@
+package com.example.enviauoc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Nuevo_Envio extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Fichero xml que define la interfaz grafica
+		setContentView(R.layout.nuevo_envio);
+	}
+}
