4a6c145512b780287b45ca479771f5866f3ff5eb
sergioromerogarcia <sergioromero@pcport59.atlantis-seguros.es>
2014-12-10 11:57:57 +0100

    Entregable v2
    Revisado

--- src/com/example/enviauoc/MainActivity.java
+++ src/com/example/enviauoc/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends Activity {
 		}
 		// Define el campo de Usuario y lo relaciona con el id del fichero activity.xml
 		textUsuario = (EditText) findViewById(R.id.editText1);
+		textUsuario.requestFocus();
 		// Define el campo del Passw y lo relaciona con el id del fichero activity.xml
 		textPassword = (EditText) findViewById(R.id.editText2);
 		
